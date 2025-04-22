ce90a8e89650eca9477975ea0b811df538062c5b
Pratap Singh <ranaprataps189@gmail.com>
2025-04-23 00:52:16 +0530

    solved quiz question of Apna clg

---
+++ Day1/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+
+public class AreaOfRectangle {
+ public static void main(String[] args) {
+    int lenght = 4;
+    int height = 8;
+    int area = lenght * height;  // This calculates the area of the rectangle
+    System.out.println("area of rectangle is = " + area);
+
+ } 
+}
