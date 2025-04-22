9dd11057dfa01c8bbf0888e672ceb12c96bbc7c2
Pratap Singh <ranaprataps189@gmail.com>
2025-04-23 00:38:56 +0530

    This is my first java class problem question

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        String Name = "Rana Pratap Singh";
+        int marks = 378;
+        int age = 20;
+        int StudentId = 445-11356;
+        int grade = 85;
+        float StudentFee = 15000.00f;
+        System.out.println("Student Name: " + Name + ", Marks: " + marks + ", Age: " + age + ", Student ID: " + StudentId + ", Grade: " + grade + ", Fee: " + StudentFee);
+    }
+}
