d1ae89fa1fb93172ec68fd8a0a9db95929839f5b
Pratap Singh <ranaprataps189@gmail.com>
2025-05-29 09:37:28 +0530

    boolean understanding

---
+++ Day4/dataTypes/Booleans.java
@@ -0,0 +1,20 @@
+// package Only-java.Day4.dataTypes;
+
+public class Booleans {
+    public static void main(String[] args) {
+        /*
+         Booleans => you will need a data type that can only have one or two value like 
+         YES/NO
+         TRUE/FALSE
+         ON/OFF
+
+         JAVA has a boolean data type that which can only take the value  "true or false"
+
+         */
+
+
+         boolean isJavaFun = true;
+         boolean isFishtasty = false;
+         System.out.println(isFishtasty);
+    }
+}
