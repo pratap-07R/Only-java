259c36578feb89af61f961835a3850faa2731202
Pratap Singh <ranaprataps189@gmail.com>
2025-05-29 17:54:10 +0530

    java Math method

---
+++ Math/javaMath.java
@@ -0,0 +1,26 @@
+// package Only-java.Math;
+
+public class javaMath {
+    public static void main(String[] args) {
+        /*
+         JAVA math class has many method that allow you to performmathematical task on computer
+         */
+
+         //math.max(a,b)
+         int val1 = Math.max(23, 87);
+         System.out.println(val1);
+
+
+         //Math.min(a,b)
+         System.out.println(Math.min(23,12));
+
+         //Math.sqrt(x)
+         System.out.println(Math.sqrt(val1));
+
+         //Math.abs(x) -> method return th absolute positive value of x
+         System.out.println(Math.abs(-4.9));
+
+         //Math.random()
+         System.out.println(Math.random()*100);
+    }
+}
