ffee44722bce62d9016bf7ae1299f0cdf61a6770
Pratap Singh <ranaprataps189@gmail.com>
2025-05-29 09:47:52 +0530

    character understanding

---
+++ Day4/dataTypes/Character.java
@@ -0,0 +1,25 @@
+// package Only-java.Day4.dataTypes;
+
+public class Character {
+    public static void main(String[] args) {
+        /*
+         CHARACTER 
+         THE char data type used to store a single character. the charcater should must be shorunded by single qoutes like 'A'
+
+         */
+
+         char myGrade = 'A';
+         char myFriendGrade = 'B';
+         System.out.println(myFriendGrade);
+    
+
+         // Alternatively if you are familiar with ASCII values you can use those to display cirtain characters 
+         char myVar1 = 65;
+         char myVar2 = 73;
+         System.out.println(myVar2);
+
+
+
+    }
+    
+}
