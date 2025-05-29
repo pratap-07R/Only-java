07b52e8de1640ac9e17e126957636764a3afd796
Pratap Singh <ranaprataps189@gmail.com>
2025-05-29 17:52:14 +0530

    adding a example

--- Day4/dataTypes/Booleans.java
+++ Day4/dataTypes/Booleans.java
@@ -16,5 +16,17 @@ public class Booleans {
          boolean isJavaFun = true;
          boolean isFishtasty = false;
          System.out.println(isFishtasty);
+
+         //REAL LIFE EXAMPLE
+         int myAge = 22;
+         int riyaAge = 21;
+
+         if(myAge>= riyaAge){
+            System.out.println("you are older than riya");
+         } else{
+            System.out.println("riya is cute");
+         }
+
+         System.out.println("yes i know there is know sense in this else part but but but");
     }
 }
