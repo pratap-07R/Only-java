eb15510894e04e2cf0dd0ddca5e62b24a4b1c160
Pratap Singh <ranaprataps189@gmail.com>
2025-06-06 23:02:00 +0530

    weightConverter

---
+++ Questions/weightConvert.java
@@ -0,0 +1,43 @@
+
+// import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class weightConvert {
+
+    public static void main(String[] args) {
+        
+   
+    
+
+    //DECLARE VARIABL
+    Scanner sc = new Scanner(System.in);
+
+    double weight;
+    double newWeight;
+    int choice;
+
+    //WELCOME MESSAGE
+    System.out.println("weight convertion program"); 
+    System.out.println("1: kgs to lbs ");
+    System.out.println("2: lbs to kgs  ");
+
+    System.out.println("choose your option ");
+    choice = sc.nextInt();
+    System.out.println("you choose "+ choice);
+
+    
+    
+
+
+    //PROMPT FOR USER CHOICE 
+
+    //OPTION 1 TO kgs TO lbs
+
+    //OPTION 2 TO lbs TO kgs
+
+    //ELSE PART 
+    }
+
+    
+}
